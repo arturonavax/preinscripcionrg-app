00754330680108c583af43e0d6807b115e0095e0
Arturo Nava <arthurnavah@gmail.com>
2018-01-17 20:16:05 -0400

    Estructura del Modelo Student terminada

--- models/students/student.go
+++ models/students/student.go
@@ -9,7 +9,25 @@ type Student struct {
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
 
+	//IDs
 	UserID uint `json:"userId" gorm:"not null;type:integer"`
+
+	CountryOfBirthID      int `json:"countryOfBirthID" gorm:"not null;type:integer"`
+	StateOfBirthID        int `json:"stateOfBirthID" gorm:"not null;type:integer"`
+	MunicOfBirthID        int `json:"municOfBirthID" gorm:"not null;type:integer"`
+	MunicipalityID        int `json:"municipalityID" gorm:"not null;type:integer"`
+	InstitutionOfOriginID int `json:"institutionOfOriginID" gorm:"not null;type:integer"`
+	ParishID              int `json:"parishID" gorm:"not null;type:integer"`
+	SectorID              int `json:"sectorID" gorm:"not null;type:integer"`
+	TypeOfRoadID          int `json:"typeOfRoadID" gorm:"not null;type:integer"`
+	FatherID              int `json:"fatherID" gorm:"not null;type:integer"`
+	MotherID              int `json:"motherID" gorm:"not null;type:integer"`
+	MentionID             int `json:"mentionID" gorm:"not null;type:integer"`
+	SectionsID            int `json:"sectionID" gorm:"not null;type:integer"`
+	StudentConditionID    int `json:"studentConditionID" gorm:"not null; type:integer"`
+	RepresentativeID      int `json:"representativeID" gorm:"not null; type:integer"`
+	TeacherID             int `json:"teacherID" gorm:"not null; type:integer"`
+	//-----
 
 	// Datos del Estudiante.
 	SIGECOD string `json:"SIGECOD" gorm:"type:varchar(20)"`
@@ -20,45 +38,31 @@ type Student struct {
 	CIType string `json:"ciType" gorm:"not null;type:char(1)"`
 	CI     int    `json:"ci" gorm:"not null; unique;integer"`
 
-	DateOfBirth    string `json:"dateOfBirth" gorm:"not null;type:date"`
-	CountryOfBirth string `json:"countryOfBirth" gorm:"not null;type:varchar(30)"`
-	StateOfBirth   string `json:"stateOfBirth" gorm:"not null;type:varchar(30)"`
-	MunicOfBirth   string `json:"municOfBirth" gorm:"not null;type:varchar(30)"`
+	DateOfBirth string `json:"dateOfBirth" gorm:"not null;type:date"`
 
-	Gender         string `json:"gender" gorm:"not null;type:char(1)"`
-	CampusOfOrigin string `json:"campusOfOrigin" gorm:"not null;type:varchar(50)"`
+	Gender string `json:"gender" gorm:"not null;type:char(1)"`
+
 	HealthProblem  string `json:"healthProblem" gorm:"not null;type:boolean"`
 	HealthProblemE string `json:"healthProblemE" gorm:"not null;type:varchar(300)"`
 
 	Email       string `json:"email" gorm:"not null;type:varchar(30)"`
 	PhoneNumber string `json:"phoneNumber" gorm:"not null;type:varchar(12)"`
 
-	Address      string `json:"address" gorm:"not null;type:varchar(30)"`
-	Municipality string `json:"municipality" gorm:"not null;type:varchar(30)"`
-	Parish       string `json:"parish" gorm:"not null;type:varchar(30)"`
-	Sector       string `json:"sector" gorm:"not null;type:varchar(30)"`
-	TypeOfRoad   string `json:"typeOfRoad" gorm:"not null;type:varchar(30)"`
-	Scholarship  string `json:"scholarship" gorm:"not null;type:boolean"`
-	Canaima      string `json:"canaima" gorm:"not null;type:boolean"`
+	Address     string `json:"address" gorm:"not null;type:varchar(30)"`
+	Scholarship string `json:"scholarship" gorm:"not null;type:boolean"`
+	Canaima     string `json:"canaima" gorm:"not null;type:boolean"`
 
 	ConditionOfHousing string `json:"conditionOfHousing" gorm:"not null;type:varchar(30)"`
 
-	FirstNameM   string `json:"firstNameM" gorm:"not null;type:varchar(50)"`
-	LastNameM    string `json:"lastNameM" gorm:"not null;type:varchar(50)"`
-	PhoneNumberM string `json:"phoneNumberM" gorm:"not null;type:varchar(12)"`
-	CIM          int    `json:"cim" gorm:"not null; unique;integer"`
+	//Escolaridad-
+	Year int `json:"year" gorm:"not null;type:integer"`
 
-	FirstNameP   string `json:"firstNameP" gorm:"not null;type:varchar(50)"`
-	LastNameP    string `json:"lastNameP" gorm:"not null;type:varchar(50)"`
-	PhoneNumberP string `json:"phoneNumberP" gorm:"not null;type:varchar(12)"`
-	CIP          int    `json:"cip" gorm:"not null; unique;integer"`
-
-	//Escolaridad
-	Year    int    `json:"year" gorm:"not null;type:integer`
-	Mention string `json:"mention" gorm:"not null;type:varchar(50)`
-	Section string `json:"section" gorm:"not null; type:varchar(10)`
-
-	Age    int    `json:"age" gorm:"not null;type:integer`
-	Size   string `json:"size" gorm:"not null;type:varchar(10)`
+	Age    int    `json:"age" gorm:"not null;type:integer"`
+	Size   string `json:"size" gorm:"not null;type:varchar(10)"`
 	Weight int    `json:"weight" gorm:"not null;type:integer"`
+
+	RepeatAsignature  []Asignature `json:"repeatAsignature"`
+	PendingAsignature []Asignature `json:"pendingAsignature"`
+	Regular           bool         `json:"regular" gorm:"not null;type:boolean"`
+	InscriptionDate   time.Time    `json:"inscriptionDate" gorm:"not null;type:date"`
 }
