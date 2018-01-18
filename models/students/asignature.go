acb655f2bbf575b0374fc42d418a35177041e4bc
Arturo Nava <arthurnavah@gmail.com>
2018-01-17 20:14:24 -0400

    Se creo el modelo para asignaturas

---
+++ models/students/asignature.go
@@ -0,0 +1,14 @@
+package student
+
+import "time"
+
+//Asignature Asignatura de la aplicacion.
+type Asignature struct {
+	ID        uint `json:"id" gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+
+	UserID uint   `json:"userID" gorm:"type:integer"`
+	Name   string `json:"name" gorm:"not null;unique;type:varchar(50)"`
+}
