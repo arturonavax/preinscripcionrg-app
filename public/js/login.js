cad853d1f2668c1e7726531e9b7808493cd8ad61
Arturo Nava <arthurnavah@gmail.com>
2018-02-09 08:59:57 -0400

    Primera Query hacia GraphQL relacionado con la informacion de el usuario

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,43 @@
 let formLogin = $("#form-login"),
-    username = $("#username"),
-    passw = $("#password"),
-    btnSubmit = $("#btn-submit"),
-    formMessage = $("#form-message");
+username = $("#username"),
+passw = $("#password"),
+btnSubmit = $("#btn-submit"),
+formMessage = $("#form-message");
 
-username.value = sessionStorage.getItem("username");
 
 formLogin.addEventListener("submit", e => {
     e.preventDefault();
     btnSubmit.disabled = true;
     btnSubmit.value = "Enviando...";
-
+    
     let obj = {
         username: username.value,
         password: passw.value
     }
-
+    
     requestAjax(formLogin.method, formLogin.action, JSON.stringify(obj))
-     .then( respuesta => {
+    .then( respuesta => {
+        
+        if (respuesta.response.code === 100) {
+            
+            formMessage.textContent = "Logueo exitoso!";
+            sessionStorage.setItem("token", respuesta.response.token);
 
-         if (respuesta.response.code === 100) {
-
-             formMessage.textContent = "Logueo exitoso!";
-             sessionStorage.setItem("token", respuesta.response.token);
-             console.log(respuesta.response);
+            if (sessionStorage.getItem("token")) {
+                var data = "query {me{id,username}}";
+            
+                requestAjaxToken("POST", "/graphql", data)
+                 .then( respuesta => {
+                     console.log(respuesta.response);
+                     document.querySelector("#titleUser").textContent = "Bienvenido/a "+respuesta.response.data.me.username+"!";
+                 })
+                 .catch( error => {
+                     console.log(error);
+                 });
+            }
+            
+            username.value = sessionStorage.getItem("username");
+            console.log(respuesta.response);
 
          } else if (respuesta.response.code === 203) {
 
--- public/js/utils.js
+++ public/js/utils.js
@@ -26,3 +26,31 @@ function requestAjax(method, url, obj) {
     });
 
 }
+
+function requestAjaxToken(method, url, obj) {
+    return new Promise(function(resolver, rechazar){
+        let xhr = new XMLHttpRequest();
+
+        xhr.open(method, url, true);
+        xhr.setRequestHeader("Content-Type", "application/graphql");
+        xhr.setRequestHeader("Authorization", sessionStorage.getItem("token"));
+        xhr.withCredentials = true;
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.addEventListener("load", e => {
+            let self = e.target;
+            let respuesta = {
+                status: self.status,
+                response: JSON.parse(self.response)
+            };
+            resolver(respuesta)
+        })
+        xhr.addEventListener("error", e => {
+            let self = e.target;
+    
+            rechazar(selft);
+        });
+    
+        xhr.send(obj);
+    });
+
+}
