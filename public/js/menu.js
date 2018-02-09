6fef34a11a2bd67fef6c62ff3d48cc7642b81f11
Arianna Tapia <ariannactm@gmail.com>
2018-02-09 16:50:24 -0400

    Mejorando modularizacion del CSS y se agregaron botones de Profile y Logout

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,8 +1,15 @@
+function $(select) {
+    return document.querySelector(select);
+}
+
 var menu = $("#menu");
 var menuBtn = $("#menu__btn");
+var dark = $("#dark");
 
 menuBtn.addEventListener("click", toggleMenu);
+dark.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     menu.classList.toggle("header__nav--open");
+    dark.classList.toggle("header__nav--open");
 }
