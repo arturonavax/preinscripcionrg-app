d8e5bf1b0ab33092d673e46f82d2e4cf390f5f17
Arturo Nava <arthurnavah@gmail.com>
2018-02-09 21:49:43 -0400

    Se agrego la maqueta de profile y su JavaScript

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+if (!sessionStorage.getItem("token")) {
+    window.location.replace("/login");
+}
+
+let userID = $("#userID"),
+    username = $("#username"),
+    email = $("#email"),
+    firstName = $("#firstName"),
+    lastName = $("#lastName"),
+    address = $("#address"),
+    phoneNumber = $("#phoneNumber"),
+    kindUser = $("#kindUser"),
+    titleUser = $("#titleUser");
+
+var btnLogout = $("#btn-logout");
+
+btnLogout.addEventListener("click", function() {
+    sessionStorage.removeItem("token");
+
+    window.location.replace("/login");
+});
+    
+var data = "query {me{id,username,email,firstName,lastName,address,phoneNumber,kindID}}"; 
+
+requestAjaxToken("POST", "/graphql", data)
+ .then( respuesta => {
+    dataGraph = respuesta.response.data
+    console.log(respuesta);
+
+    userID.textContent = dataGraph.me.id;
+    username.textContent = dataGraph.me.username;
+    email.textContent = dataGraph.me.email;
+    firstName.textContent = dataGraph.me.firstName;
+    lastName.textContent = dataGraph.me.lastName;
+    address.textContent = dataGraph.me.address;
+    phoneNumber.textContent = dataGraph.me.phoneNumber;
+    kindUser.textContent = dataGraph.me.kindID;
+
+    titleUser.textContent = dataGraph.me.username;
+ })
+ .catch( error => {
+    console.log(error);
+ });
