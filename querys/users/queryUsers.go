3870e46ea2e6e67605abea26b0e248c59573fd63
Arturo Nava <arthurnavah@gmail.com>
2018-02-10 22:51:41 -0400

    Se corrio el Query Graphql para obtener la lista de usuarios

--- querys/users/queryUsers.go
+++ querys/users/queryUsers.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"github.com/arthurnavah/PreInscripcionRG/databases"
+	"github.com/arthurnavah/PreInscripcionRG/models"
 	"github.com/arthurnavah/PreInscripcionRG/models/graphqlTypes"
 	"github.com/graphql-go/graphql"
 )
@@ -8,6 +10,20 @@ import (
 var QueryUsers = &graphql.Field{
 	Type: graphql.NewList(graphqlTypes.UserType),
 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+		kind := &models.UserKind{}
+		var ListUsers []models.User
 
+		kindID := p.Context.Value("user").(models.User).KindID
+
+		db := databases.GetConnectionDB()
+		defer db.Close()
+
+		db.Where("id = ?", kindID).First(&kind)
+
+		if kind.ReadUsers {
+			db.Find(&ListUsers)
+		}
+
+		return ListUsers, nil
 	},
 }
