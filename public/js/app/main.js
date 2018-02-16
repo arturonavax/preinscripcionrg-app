b2cc3aec809295ab14f0cd43f536011aa6fa0c52
Arturo Nava <arthurnavah@gmail.com>
2018-02-16 19:38:00 -0400

    Se programo el boton back y profile, Listo el boton de cancelar edicion de perfil

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -1,3 +1,15 @@
 if (!sessionStorage.getItem("token")) {
     window.location.replace("/user");
 }
+
+let btnProfile = $("#btn-menuProfile"),
+    profileContainer = $("#container-profile"),
+    btnBack = $("#btn-back");
+
+btnProfile.addEventListener("click", function(){
+    profileContainer.classList.add("section__container--profile--open");
+});
+
+btnBack.addEventListener("click", function() {
+    profileContainer.classList.remove("section__container--profile--open");
+});
